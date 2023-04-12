f4d3382c18596a64315a960f32359e8bad85eb6e
EnKdev <sesoui72@googlemail.com>
2023-04-12 10:50:02 +0200

    Add node.js Move from discord-rpc to discord-rich-presence Code cleanup and rewrite

--- RPC.js
+++ RPC.js
@@ -1,42 +1,37 @@
-const DiscordRPC = require('discord-rpc');
-const clientId = 'CLIENT ID HERE';
+const clientConfig = require('./assets/config.json');
+const clientId = `${clientConfig.clientId}`;
+const client = require('discord-rich-presence')(clientId);
 
-// Calculate the time left until May 12th, 2023 at 12:00 PM
-const countdownDate = new Date('2023-05-12T12:00:00');
-const currentDate = new Date();
-const timeLeft = countdownDate - currentDate;
-const timeLeftInMinutes = Math.floor(timeLeft / (1000 * 60));
-const timeLeftInDays = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+// Set up the target date and get the respective time.
+let targetDate = new Date("May 12, 2023 12:00:00").getTime()
 
-// Set up the Rich Presence
-const rpc = new DiscordRPC.Client({
-    transport: 'ipc'
-});
+// Initiate the update function
+let update = setInterval(function() {
+    let currentDate = new Date().getTime();
+    let timeUntil = targetDate - currentDate;
 
-function setRPC() {
-    rpc.setActivity({
-        details: `Waiting for Tears of the Kingdom...`,
-        state: `Only ${timeLeftInDays} days left!`,
-        largeImageKey: 'totk',
-        largeImageText: 'Tears of the Kingdom',
-        smallImageKey: 'clock',
-        smallImageText: `Time left: ${timeLeftInDays} days`,
-        startTimestamp: Math.floor(currentDate.getTime() / 1000),
-        endTimestamp: Math.floor(countdownDate.getTime() / 1000),
+    // Get the remaining days, hours and minutes
+    let days = Math.floor(timeUntil / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeUntil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeUntil % (1000 * 60 * 60)) / (1000 * 60));
+
+    console.log(`${days}d ${hours}h ${minutes}m`);
+    setRpc(days, hours, minutes);
+}, 1000);
+
+function setRpc(days, hours, minutes) {
+    client.updatePresence({
+        details: "Waiting for Tears of the Kingdom...",
+        state: `${days}d ${hours}h ${minutes}m left!`,
+        largeImageKey: "totk",
+        largeImageText: "Tears of the Kingdom",
+        smallImageKey: "clock",
+        smallImageText: `Time left: ${days}d ${hours}h ${minutes}m`,
         buttons: [
-            { label: 'Watch the Countdown', url: 'https://www.timeanddate.com/countdown/generic?iso=20230512T00&p0=1309&msg=Time+until+Tears+of+the+Kingdom+releases...&ud=1&font=slab&csz=1#' }
-          ]
+            {
+                label: "Watch the countdown",
+                url: "https://www.timeanddate.com/countdown/generic?iso=20230512T00&p0=1309&msg=Time+until+Tears+of+the+Kingdom+releases...&ud=1&font=slab&csz=1#"
+            }
+        ]
     });
 }
-
-rpc.on('ready', () => {
-    console.log('Connected to Discord');
-    setRPC();
-    setInterval(() => {
-        setRPC();
-    }, 60 * 60 * 1000); // Update every hour
-});
-
-rpc.login({clientId}).catch((error) => {
-    console.error(error);
-});
